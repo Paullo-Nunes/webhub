17b05091abed285d056d9a8c24a3d1cf5c9ec3e2
Dev Js <alienwaredev369@outlook.com>
2026-06-05 21:24:38 -0300

    Incrementado o vídeo ao footer usando o javascript

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 window.addEventListener('load', () => {
     const videoHero = document.querySelector(".hero .bg video");
+    
     videoHero.src = "/assets/img/video-hero.mp4";
     videoHero.autoplay = true;
     videoHero.loop = true;
     videoHero.muted = true;
 
+    const videoFooter = document.querySelector("footer video");
+    
+    videoFooter.src = "/assets/img/video-footer.mp4";
+    videoFooter.autoplay = true;
+    videoFooter.loop = true;
+    videoFooter.muted = true;
+
 })
+
